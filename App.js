9690a5e61c2c2b59884e105b1e26e3dbd0a90762
antonio santos <antoniosantos.devweb@gmail.com>
2024-03-11 20:37:21 -0300

    calculator react native

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, View, Text, TextInput, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const [result, setResult] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={setFirstValue}
+        value={firstValue}
+        keyboardType='numeric'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setSecondValue}
+        value={secondValue}
+        keyboardType='numeric'
+      />
+      <Button
+        title="Adicionar"
+        onPress={() => setResult(Number(firstValue) + Number(secondValue))}
+      />
+      <Button
+        title="Subtrair"
+        onPress={() => setResult(Number(firstValue) - Number(secondValue))}
+      />
+      <Button
+        title="Multiplicar"
+        onPress={() => setResult(Number(firstValue) * Number(secondValue))}
+      />
+      <Button
+        title="Dividir"
+        onPress={() => setResult(Number(firstValue) / Number(secondValue))}
+      />
+      <Text>Resultado: {result}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
   },
 });
+
+export default App;
